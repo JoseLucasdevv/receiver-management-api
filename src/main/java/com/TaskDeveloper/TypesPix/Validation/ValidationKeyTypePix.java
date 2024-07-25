6b917066d22e14dd31a98fb43d0872aa2ba353d7
Jose Lucas <joselucas.dev@gmail.com>
2024-07-25 09:18:14 -0300

    validation:Create ValidationKeyType and value

---
+++ src/main/java/com/TaskDeveloper/TypesPix/Validation/ValidationKeyTypePix.java
@@ -0,0 +1,65 @@
+package com.TaskDeveloper.TypesPix.Validation;
+
+import com.TaskDeveloper.Dtos.ReceiverDTO;
+import com.TaskDeveloper.TypesPix.TypesPix;
+import org.springframework.stereotype.Component;
+
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+@Component
+public class ValidationKeyTypePix {
+
+
+    public void handlereceiverDTO(ReceiverDTO receiverdto){
+
+
+        receiverdto.pix().forEach(receiverPix -> {
+             this.ValidationKey(receiverPix.getKey_type(), receiverPix.getKey_pix());
+        });
+
+    }
+
+private Boolean ValidationKey(TypesPix keyType,String keyValue)  {
+
+    switch(keyType){
+        case CPF:
+            Boolean matcherCPF = matcherPattern("^[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}-?[0-9]{2}$", keyValue);
+            System.out.println(matcherCPF);
+            return matcherCPF;
+
+        case CNPJ:
+            Boolean matcherCNPJ = matcherPattern("^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}\\/?[0-9]{4}-?[0-9]{2}$", keyValue);
+            System.out.println(matcherCNPJ);
+            return matcherCNPJ;
+
+        case RANDOM_KEY:
+            Boolean matcherRandomKey = matcherPattern("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", keyValue);
+            System.out.println(matcherRandomKey);
+            return matcherRandomKey;
+
+        case PHONE:
+            Boolean matcherPhone = matcherPattern("^((?:\\+?55)?)([1-9][0-9])(9[0-9]{8})$", keyValue);
+            System.out.println(matcherPhone);
+            return matcherPhone;
+
+        case EMAIL:
+            Boolean matcherEmail = matcherPattern("^[a-z0-9+_.-]+@[a-z0-9.-]+$", keyValue);
+            System.out.println(matcherEmail);
+            return matcherEmail;
+
+        default:
+            return false;
+
+        }
+    }
+
+    private boolean matcherPattern(String patternKey , String keyValue){
+        Pattern pattern = Pattern.compile(patternKey);
+        Matcher matcher = pattern.matcher(keyValue);
+        return matcher.matches();
+    }
+
+}
