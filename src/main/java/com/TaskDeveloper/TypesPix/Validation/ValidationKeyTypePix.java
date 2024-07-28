0fb7e131f90b7eda3100b268cd4dca85c8507524
Jose Lucas <joselucas.dev@gmail.com>
2024-07-28 11:25:29 -0300

    refactor:Finally to clean setMemory after verification

--- src/main/java/com/TaskDeveloper/TypesPix/Validation/ValidationKeyTypePix.java
+++ src/main/java/com/TaskDeveloper/TypesPix/Validation/ValidationKeyTypePix.java
@@ -11,30 +11,35 @@ import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-
-
 @Component
 public class ValidationKeyTypePix {
 
     private Set<String> processDuplicateKey = new HashSet<>();
 
     public void handlereceiverDTO(ReceiverDTO receiverdto){
+        try{
         receiverdto.pix().forEach(receiverPix -> {
                 this.checkForDuplicateKey(receiverPix.getKey_pix());
                 this.validationKey(receiverPix.getKey_type(), receiverPix.getKey_pix());
         });
-
+        }
+        finally {
+        this.resetProcessedKeys();
+        }
     }
     public void handlereceiverDTOList(List<ReceiverDTO> receiverdto){
-
+        try{
         receiverdto.forEach(receivers->{
             receivers.pix().forEach(receiverPix ->{
                 this.checkForDuplicateKey(receiverPix.getKey_pix());
                 this.validationKey(receiverPix.getKey_type(), receiverPix.getKey_pix());
             });
         });
-
+        }finally {
+            this.resetProcessedKeys();
+        }
     }
+
     private void checkForDuplicateKey(String keyValue) {
         if (processDuplicateKey.contains(keyValue)) {
             throw new Exception("Duplicate Key: " + keyValue);
@@ -43,38 +48,39 @@ public class ValidationKeyTypePix {
         }
     }
 
+    private void resetProcessedKeys() {
+        processDuplicateKey.clear();
+    }
 
-
-    private Boolean validationKey(TypesPix keyType,String keyValue)  {
+    private void validationKey(TypesPix keyType, String keyValue)  {
 
     switch(keyType){
         case CPF:
             Boolean matcherCPF = matcherPattern("^[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}-?[0-9]{2}$", keyValue);
             System.out.println(matcherCPF);
-
             this.checkMatcher(matcherCPF);
 
-            return matcherCPF;
+            return;
 
         case CNPJ:
             Boolean matcherCNPJ = matcherPattern("^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}\\/?[0-9]{4}-?[0-9]{2}$", keyValue);
             System.out.println(matcherCNPJ);
-            return matcherCNPJ;
+            return;
 
         case RANDOM_KEY:
             Boolean matcherRandomKey = matcherPattern("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", keyValue);
             System.out.println(matcherRandomKey);
-            return matcherRandomKey;
+            return;
 
         case PHONE:
             Boolean matcherPhone = matcherPattern("^((?:\\+?55)?)([1-9][0-9])(9[0-9]{8})$", keyValue);
             System.out.println(matcherPhone);
-            return matcherPhone;
+            return;
 
         case EMAIL:
             Boolean matcherEmail = matcherPattern("^[a-z0-9+_.-]+@[a-z0-9.-]+$", keyValue);
             System.out.println(matcherEmail);
-            return matcherEmail;
+            return;
 
         default:
             throw new Exception("Unknown KeyType: " + keyType);
@@ -88,12 +94,10 @@ public class ValidationKeyTypePix {
         return matcher.matches();
     }
 
-    private Boolean checkMatcher(Boolean matcherCPF){
-
+    private void checkMatcher(Boolean matcherCPF){
         if(!matcherCPF){
             throw new Exception("Invalid KeyType");
         }
-        return true;
     };
 
 }
