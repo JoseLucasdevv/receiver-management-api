68445be4d4ff157065d82074727905f3b1272f74
Jose Lucas <joselucas.dev@gmail.com>
2024-07-30 09:38:05 -0300

    Mapper:ReceiverMapper status

--- src/main/java/com/TaskDeveloper/Mappers/ReceiverMapper.java
+++ src/main/java/com/TaskDeveloper/Mappers/ReceiverMapper.java
@@ -1,15 +1,15 @@
 package com.TaskDeveloper.Mappers;
 import com.TaskDeveloper.Dtos.ReceiverDTO;
 import com.TaskDeveloper.Entity.Receiver;
+import com.TaskDeveloper.TypesStatus.TypeStatus;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReceiverMapper {
 
     public ReceiverDTO entityToDTO(Receiver receiver){
-        return new ReceiverDTO(receiver.getReceiverId(),receiver.getName(),receiver.getCpf(),receiver.getEmail(),receiver.getPix());
+        return new ReceiverDTO(receiver.getReceiverId(),receiver.getName(),receiver.getCpf(),receiver.getEmail(),receiver.getPix(),receiver.getStatus());
     }
 
     public Receiver dtoToEntity(ReceiverDTO receiverdto){
@@ -18,6 +18,10 @@ public class ReceiverMapper {
         receiver.setName(receiverdto.name());
         receiver.setEmail(receiverdto.email());
         receiver.setPix(receiverdto.pix());
+        if(receiverdto.status() == TypeStatus.VALIDATED){
+            receiver.setStatus(receiverdto.status());
+        }
+
         return receiver;
     }
     public List<Receiver> listDtoToEntity(List<ReceiverDTO> receiverdto) {
@@ -28,9 +32,11 @@ public class ReceiverMapper {
             receiver.setName(dto.name());
             receiver.setEmail(dto.email());
             receiver.setPix(dto.pix());
+            if(dto.status() == TypeStatus.VALIDATED){
+                receiver.setStatus(dto.status());
+            }
             return receiver;
         }).collect(Collectors.toList());
-
 
     }
 
