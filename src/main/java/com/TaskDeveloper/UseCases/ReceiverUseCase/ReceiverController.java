d6bd8f82ab693fd4a311b4249c3f6cd55bb965c5
Jose Lucas <joselucas.dev@gmail.com>
2024-07-30 10:30:39 -0300

    feat:add new Methods layer Controller findByName and findByStatus

--- src/main/java/com/TaskDeveloper/UseCases/ReceiverUseCase/ReceiverController.java
+++ src/main/java/com/TaskDeveloper/UseCases/ReceiverUseCase/ReceiverController.java
@@ -2,6 +2,7 @@ package com.TaskDeveloper.UseCases.ReceiverUseCase;
 
 import com.TaskDeveloper.Dtos.ReceiverDTO;
 import com.TaskDeveloper.Entity.Receiver;
+import com.TaskDeveloper.TypesStatus.TypeStatus;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -39,10 +40,25 @@ public class ReceiverController {
     }
     @GetMapping("{pageNumber}")
     @ResponseBody
-    public ResponseEntity<List<Receiver>> getAllResources(@PathVariable int pageNumber){
+    public ResponseEntity<List<Receiver>> getAllReceivers(@PathVariable int pageNumber){
 
-     return new ResponseEntity<>(this._getReceiverUseCase.resourceAllReceiver(pageNumber), HttpStatus.OK);
+     return new ResponseEntity<>(this._getReceiverUseCase.getAllReceiver(pageNumber), HttpStatus.OK);
     }
+    @GetMapping("name/{name}/{pageNumber}")
+    @ResponseBody
+    public ResponseEntity<List<Receiver>> getAllReceiversByName(@PathVariable String name,@PathVariable int pageNumber){
+
+        return new ResponseEntity<>(this._getReceiverUseCase.getAllReceiverByName(name,pageNumber), HttpStatus.OK);
+    }
+
+    @GetMapping("status/{status}/{pageNumber}")
+    @ResponseBody
+    public ResponseEntity<List<Receiver>> getAllReceiversByStatus(@PathVariable int pageNumber,@PathVariable TypeStatus status){
+
+        return new ResponseEntity<>(this._getReceiverUseCase.getAllReceiverByStatus(pageNumber,status), HttpStatus.OK);
+    }
+
+
     @PutMapping("{id}")
     @ResponseBody
     public ResponseEntity<String> updateReceiver(@PathVariable long id, @Valid @RequestBody ReceiverDTO receiverdto){
