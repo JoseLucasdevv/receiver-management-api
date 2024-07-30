ef36e56503715e20eea8589a6a89394c6726bde0
Jose Lucas <joselucas.dev@gmail.com>
2024-07-30 10:30:13 -0300

    feat:add new Methods layer repository findByName and findByStatus

--- src/main/java/com/TaskDeveloper/Repository/ReceiverRepository.java
+++ src/main/java/com/TaskDeveloper/Repository/ReceiverRepository.java
@@ -1,6 +1,7 @@
 package com.TaskDeveloper.Repository;
 
 import com.TaskDeveloper.Entity.Receiver;
+import com.TaskDeveloper.TypesStatus.TypeStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import java.util.List;
 
 public interface ReceiverRepository extends JpaRepository<Receiver,Long> {
     Page<Receiver> findAll(Pageable page);
+    Page<Receiver> findAllByName(String name,Pageable page);
+    Page<Receiver> findAllByStatus(TypeStatus status,Pageable page);
 }
