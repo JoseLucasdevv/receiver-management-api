6dc381871ac3474f99478f2d5fba49fd95775982
jose-lucas <joselucas.dev@gmail.com>
2024-08-10 08:50:28 -0300

    feat: add new validation method valiateCpfOrCnpj and refactor patterns.

--- src/main/java/com/TaskDeveloper/TypesPix/Validation/Validations.java
+++ src/main/java/com/TaskDeveloper/TypesPix/Validation/Validations.java
@@ -13,7 +13,13 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 @Component
-public class ValidationKeyTypePix {
+public class Validations {
+
+    private String cpfPattern = "^[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}-?[0-9]{2}$";
+    private String cnpjPattern = "^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}\\/?[0-9]{4}-?[0-9]{2}$";
+    private String randomKeyPattern = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+    private String phonePattern = "^((?:\\+?55)?)([1-9][0-9])(9[0-9]{8})$";
+    private String emailPattern = "^[a-z0-9+_.-]+@[a-z0-9.-]+$";
 
     private Set<String> processDuplicateKey = new HashSet<>();
 
@@ -57,33 +63,29 @@ public class ValidationKeyTypePix {
 
     switch(keyType){
         case CPF:
-
-            Boolean matcherCPF = matcherPattern("^[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}-?[0-9]{2}$", keyValue);
+            Boolean matcherCPF = matcherPattern(this.cpfPattern, keyValue);
             this.checkMatcher(matcherCPF,keyType);
-
             return;
 
         case CNPJ:
-
-            Boolean matcherCNPJ = matcherPattern("^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}\\/?[0-9]{4}-?[0-9]{2}$", keyValue);
+            Boolean matcherCNPJ = matcherPattern(this.cnpjPattern, keyValue);
             this.checkMatcher(matcherCNPJ,keyType);
             return;
 
         case RANDOM_KEY:
-
-            Boolean matcherRandomKey = matcherPattern("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", keyValue);
+            Boolean matcherRandomKey = matcherPattern(this.randomKeyPattern, keyValue);
             this.checkMatcher(matcherRandomKey,keyType);
             return;
 
         case PHONE:
 
-            Boolean matcherPhone = matcherPattern("^((?:\\+?55)?)([1-9][0-9])(9[0-9]{8})$", keyValue);
+            Boolean matcherPhone = matcherPattern(this.phonePattern, keyValue);
             this.checkMatcher(matcherPhone,keyType);
             return;
 
         case EMAIL:
 
-            Boolean matcherEmail = matcherPattern("^[a-z0-9+_.-]+@[a-z0-9.-]+$", keyValue);
+            Boolean matcherEmail = matcherPattern(this.emailPattern, keyValue);
             this.checkMatcher(matcherEmail,keyType);
             return;
 
@@ -96,6 +98,33 @@ public class ValidationKeyTypePix {
         Pattern pattern = Pattern.compile(patternKey);
         Matcher matcher = pattern.matcher(keyValue);
         return matcher.matches();
+    }
+
+    public void ValidateCpfOrCnpj(ReceiverDTO receiver){
+    Boolean cpfValidate =  this.matcherPattern(this.cpfPattern,receiver.cpfOrCnpj());
+    Boolean cnpjValidate = this.matcherPattern(this.cnpjPattern,receiver.cpfOrCnpj());
+
+    if(cpfValidate || cnpjValidate){
+        System.out.println("ok");
+    }else{
+        throw new Exception("Cpf or CNPJ failed");
+    }
+
+    }
+
+    public void ValidateCpfOrCnpjList(List<ReceiverDTO> receiverDto){
+        receiverDto.forEach(r ->{
+            Boolean cpfValidate =  this.matcherPattern(this.cpfPattern,r.cpfOrCnpj());
+            Boolean cnpjValidate = this.matcherPattern(this.cnpjPattern,r.cpfOrCnpj());
+
+            if(cpfValidate || cnpjValidate){
+                System.out.println("ok");
+            }else{
+                throw new Exception("Cpf or CNPJ failed");
+            }
+
+        });
+
     }
 
     private void checkMatcher(Boolean matcher, TypesPix type){
