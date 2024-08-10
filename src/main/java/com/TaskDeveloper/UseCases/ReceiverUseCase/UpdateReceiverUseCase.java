7ac9565841e92782f4cfa34ec3c34cbe1cba4c46
jose-lucas <joselucas.dev@gmail.com>
2024-08-10 08:49:45 -0300

    feat: add business logic UpdateReceiverUseCase

--- src/main/java/com/TaskDeveloper/UseCases/ReceiverUseCase/UpdateReceiverUseCase.java
+++ src/main/java/com/TaskDeveloper/UseCases/ReceiverUseCase/UpdateReceiverUseCase.java
@@ -1,11 +1,18 @@
 package com.TaskDeveloper.UseCases.ReceiverUseCase;
 
 import com.TaskDeveloper.Dtos.ReceiverDTO;
+import com.TaskDeveloper.Entity.Pix;
 import com.TaskDeveloper.Entity.Receiver;
 import com.TaskDeveloper.Mappers.ReceiverMapper;
 import com.TaskDeveloper.Repository.ReceiverRepository;
+import com.TaskDeveloper.TypesPix.Validation.Validations;
+import com.TaskDeveloper.TypesStatus.TypeStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.TaskDeveloper.exceptions.Exception;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -13,17 +20,40 @@ public class UpdateReceiverUseCase {
 
     @Autowired
     ReceiverRepository _receiverRepository;
+    @Autowired
+    Validations _validations;
 
     public String updateReceiver(long id , ReceiverDTO receiverdto){
-
+        Receiver receiver = new ReceiverMapper().dtoToEntity(receiverdto);
        this._receiverRepository.findById(id).ifPresentOrElse((user)->{
 
-           Receiver receiver = new ReceiverMapper().dtoToEntity(receiverdto);
+           if(user.getStatus() != receiver.getStatus()){
+               throw new Exception("Cannot Change Status");
+           }
 
-           user.setPix(receiver.getPix());
-           user.setCpf(receiver.getCpf());
-           user.setName(receiver.getName());
-           user.setEmail(receiver.getEmail());
+           if(user.getStatus() == TypeStatus.VALIDATED){
+               this.checkPix(user.getPix(),receiver.getPix());
+
+                if(!user.getCpfOrCnpj().equals(receiver.getCpfOrCnpj())){
+                    throw new Exception("Cannot change cpfOrCnpj");
+                }
+               if(!user.getName().equals(receiver.getName())){
+                   throw new Exception("Cannot change Name");
+               }
+
+                user.setEmail(receiver.getEmail());
+           }
+
+           if(user.getStatus() == TypeStatus.SKETCH){
+            this._validations.handlereceiverDTO(receiverdto);
+
+               user.setPix(receiver.getPix());
+               user.setCpfOrCnpj(receiver.getCpfOrCnpj());
+               user.setName(receiver.getName());
+               user.setEmail(receiver.getEmail());
+
+           }
+
 
            this._receiverRepository.save(user);
 
@@ -32,6 +62,30 @@ public class UpdateReceiverUseCase {
             });
 
         return "Success to update user";
+
+    }
+    private void checkPix(List<Pix> bdPix, List<Pix> inputPix){
+    List<String> bdKeys = new ArrayList<>();
+    List<String> inputKeys = new ArrayList<>();
+
+
+        bdPix.forEach(pix -> {
+            bdKeys.add(pix.getKey_type().name());
+            bdKeys.add(pix.getKey_pix());
+        });
+        inputPix.forEach(pix -> {
+            inputKeys.add(pix.getKey_type().name());
+            inputKeys.add(pix.getKey_pix());
+
+        });
+
+        if(!bdKeys.get(0).equals(inputKeys.get(0))){
+            throw new Exception("Cannot change Pix key");
+        }
+        if(!bdKeys.get(1).equals(inputKeys.get(1))){
+            throw new Exception("cannot change Pix value");
+        }
+
 
     }
 
